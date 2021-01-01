c31bbfb980fa48a8de89d391de6ee9a8132ecbde
Kinara Safina <kinarasafina@Kinara-Safina.local>
2021-01-01 11:40:52 +0700

    added .env and styled the page

--- src/components/City.js
+++ src/components/City.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import "../Home.css";
 
 // props -> cityName ,temp, color
-function City({ cityName, temp, color }) {
+function City({ cityName, weatherType, temp = 0, color }) {
   const history = useHistory();
 
   function handleClick() {
@@ -11,11 +12,17 @@ function City({ cityName, temp, color }) {
 
   return (
     <button
-      className={"flex flex-row p-8 justify-between items-center " + color}
+      className={
+        "flex flex-row p-8 justify-between items-center cities " + color
+      }
       onClick={handleClick}
     >
-      <div className="text-2xl font-bold">{cityName}</div>
-      <div className="text-5xl font-thin">{temp}</div>
+      <div className="flex flex-col cities">
+        <div className="text-md font-thin text-left">{weatherType}</div>
+        <div className="text-2xl font-bold">{cityName}</div>
+      </div>
+
+      <div className="text-5xl font-thin cities">{Math.round(temp)}°C</div>
     </button>
   );
 }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -14,6 +14,51 @@ function Home() {
 
   const [city, setCity] = useState("Jakarta");
 
+  const [cities, setCities] = useState([
+    {
+      name: "Jakarta",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-yellow-300",
+    },
+    {
+      name: "Seattle",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-yellow-200",
+    },
+    {
+      name: "New York",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-yellow-100",
+    },
+    {
+      name: "Seoul",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-yellow-300",
+    },
+    {
+      name: "Japan",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-yellow-200",
+    },
+    {
+      name: "Amsterdam",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-yellow-100",
+    },
+    {
+      name: "Singapore",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-yellow-300",
+    },
+  ]);
+
   useEffect(() => {
     // console.log(process.env.REACT_APP_WEATHER_KEY);
     axios
@@ -48,10 +93,64 @@ function Home() {
     return { currentTemp };
   }, [weatherData]);
 
+  useEffect(() => {
+    updateAllWeatherData();
+  }, []);
+
+  async function fetchWeatherData(cityName) {
+    const res = await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`
+      )
+      .then(function (response) {
+        // Successful request
+        const weather = response.data;
+        return weather;
+      })
+      .catch(function (error) {
+        // The best practice of coding is to not use console.log
+        console.warn(error);
+      });
+
+    return res;
+  }
+
+  async function updateAllWeatherData(params) {
+    cities.forEach(function (citiesItems, index) {
+      let weatherData = {};
+      let newCities = [...cities];
+
+      fetchWeatherData(citiesItems.name).then((res) => {
+        weatherData = res;
+        console.log(weatherData);
+
+        newCities[index].currentTemp = weatherData.main.temp;
+        newCities[index].weatherType = weatherData.weather[0].main;
+        setCities(newCities);
+      });
+    });
+  }
+
   return (
     <div className="h-screen bg-gray-50">
+      <div className="flex flex-col justify-center items-center pt-8">
+        <p className="text-6xl font-normal">peek</p>
+        <p className="text-2xl font-thin text-gray-700">
+          take a quick peek at the weather
+        </p>
+      </div>
+
       <div className="flex flex-col p-10">
-        <City cityName={city} temp={currentTemp} color={"bg-yellow-300"} />
+        {cities.map((item, index) => (
+          <City
+            cityName={item.name}
+            weatherType={item.weatherType}
+            temp={item.currentTemp}
+            color={item.color}
+          />
+        ))}
+
+        {/* <City cityName={city} temp={currentTemp} color={"bg-yellow-300"} /> */}
 
         {/* <City cityName={city} temp={currentTemp} color={"bg-yellow-200"} /> */}
 
